00fa0b851ceab472c2ef392953070e8380f01c0a
Davi de Oliveira Gomes <odavi4277@gmail.com>
2025-08-13 17:07:28 -0300

    feat(models, repository, tables): implementações

---
+++ internal/database/migrations/tables.sql
@@ -0,0 +1,21 @@
+-- cria tabela para os parceiros visando autenticação
+CREATE TABLE partners (
+    id INT AUTO_INCREMENT PRIMARY KEY, 
+    name VARCHAR(50) NOT NULL, 
+    api_key VARCHAR(100) NOT NULL UNIQUE -- garantir chave diferetes
+); 
+
+-- cria tabela para conversões
+CREATE TABLE conversions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    transaction_id VARCHAR(50) NOT NULL UNIQUE,  -- sem duplicatas
+    partner_id INT NOT NULL, 
+    amount DECIMAL(6, 2) NOT NULL, 
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+    FOREIGN KEY (partner_id) REFERENCES partners(id)
+
+);
+
+
+--teste
+INSERT INTO partners (name, api_key) VALUES ('ParceiroFulano', 'davi-chave-1234');
